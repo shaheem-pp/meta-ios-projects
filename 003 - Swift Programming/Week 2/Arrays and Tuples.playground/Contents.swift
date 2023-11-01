6e3eebeacbb2ab94b19dbd80d1892a3d6545adaa
shaheem-pp <shanofficial2000@gmail.com>
2023-11-01 08:06:36 +0530

    Swift Datastructures

--- 003 - Swift Programming/Week 2/Arrays and Tuples.playground/Contents.swift
+++ 003 - Swift Programming/Week 2/Arrays and Tuples.playground/Contents.swift
@@ -1,3 +1,40 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var levelScores: [Int] = []
+if levelScores.count == 0 {
+    print("Start playing the game!")
+}
+let firstLevelScore = 10
+levelScores.append(firstLevelScore)
+print("The first level's score is \(levelScores[0]).")
+let levelBonusScore = 40
+levelScores[0] += levelBonusScore
+print("The first level's score is \(levelScores[0]).")
+let freeLevelScores = [20, 30]
+levelScores += freeLevelScores
+let freeLevels = 3
+if levelScores.count == freeLevels {
+    print(
+        "You have finished playing the free version of the game. Buy the game to play its full version."
+    )
+    levelScores = []
+    print("Game restarted!")
+}
+
+
+let credentials = ("", -1111)
+if credentials.0 == "" || credentials.1 < 0 {
+    print("Invalid credentials!")
+} else {
+    print("The username is \(credentials.0) and the password is \(credentials.1).")
+}
+let fullCredentials = (password: "pass", passcode: 1111)
+if fullCredentials.password == "" || fullCredentials.passcode < 0 {
+    print("Invalid credentials!")
+} else {
+    print("The password is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode).")
+}
+
+
